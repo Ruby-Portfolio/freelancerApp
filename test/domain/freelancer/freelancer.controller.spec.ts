100ac7a8a529d8ad7253339704462b4faf6701e5
MisterRuby <ruby8700@naver.com>
2022-11-23 23:12:32 +0900

    test : 프리랜서 목록 검색 조회 테스트
    인증이 되지 않은 사용자의 요청시 401 응답
    페이지 번호가 0 이하일 경우 400 응답
    페이지 번호가 숫자가 아닐 경우 400 응답
    검색어를 입력하지 않을 경우 전체 목록을 대상으로 검색
    페이지 번호를 입력하지 않을 경우 첫 페이지를 조회
    검색어와 페이지 번호에 해당하는 목록 조회

--- test/domain/freelancer/freelancer.controller.spec.ts
+++ test/domain/freelancer/freelancer.controller.spec.ts
@@ -15,11 +15,15 @@ import * as request from 'supertest';
 import { JwtService } from '@nestjs/jwt';
 import { Payload } from '../../../src/auth/jwt/jwt.payload';
 import * as cookieParser from 'cookie-parser';
-import { FreelancerAdd } from '../../../src/domain/freelancer/freelancer.request';
+import {
+  FreelancerAdd,
+  FreelancerSearch,
+} from '../../../src/domain/freelancer/freelancer.request';
 import { pipeConfig } from '../../../src/config/pipeConfig';
 import { interceptorConfig } from '../../../src/config/interceptorConfig';
 import { FreelancerErrorMessage } from '../../../src/domain/freelancer/freelancer.message';
 import { Position } from '../../../src/domain/freelancer/freelancer.enum';
+import { CommonErrorMessage } from '../../../src/common/error/common.message';
 
 describe('FreelancerController', () => {
   let app: INestApplication;
@@ -141,6 +145,126 @@ describe('FreelancerController', () => {
         expect(freelancers[0].skills).toEqual(freelancerAdd.skills);
         expect(freelancers[0].position).toEqual(freelancerAdd.position);
         expect(freelancers[0].userId).toEqual(user.id);
+      });
+    });
+  });
+
+  describe('/GET /api/freelancers', () => {
+    describe('인증이 되지 않은 사용자의 요청', () => {
+      test('인증이 되지 않은 사용자의 요청시 401 응답', async () => {
+        await request(app.getHttpServer())
+          .get('/api/freelancers')
+          .send({
+            search: '백엔드',
+            page: 0,
+          })
+          .expect(401);
+      });
+    });
+
+    describe('인증된 사용자의 요청', () => {
+      describe('요청 실패', () => {
+        test('페이지 번호가 0 이하일 경우 400 응답', async () => {
+          const res = await request(app.getHttpServer())
+            .get('/api/freelancers')
+            .query({
+              page: -1,
+            })
+            .set('Cookie', [`Authentication=${token}`])
+            .expect(400);
+
+          expect(res.body.message).toContain(CommonErrorMessage.INVALID_PAGE);
+        });
+
+        test('페이지 번호가 숫자가 아닐 경우 400 응답', async () => {
+          const res = await request(app.getHttpServer())
+            .get('/api/freelancers')
+            .query({
+              page: '페이지 번호',
+            })
+            .set('Cookie', [`Authentication=${token}`])
+            .expect(400);
+
+          expect(res.body.message).toContain(CommonErrorMessage.INVALID_PAGE);
+        });
+      });
+      describe('요청 성공', () => {
+        beforeAll(() => {
+          for (let i = 0; i < 12; i++) {
+            freelancerRepository.insert({
+              aboutMe: `개발자 ${i}입니다.`,
+              career: `개발 경력 ${i}개월`,
+              skills: 'javaScript',
+              position: Position.FRONT_END,
+              user,
+            });
+          }
+
+          for (let i = 0; i < 12; i++) {
+            freelancerRepository.insert({
+              aboutMe: `개발자 ${i}입니다.`,
+              career: `개발 경력 ${i}개월`,
+              skills: 'C#',
+              position: Position.BACK_END,
+              user,
+            });
+          }
+        });
+
+        test('검색어를 입력하지 않을 경우 전체 목록을 대상으로 검색', async () => {
+          const freelancerSearch: FreelancerSearch = {
+            page: 0,
+          };
+
+          const res = await request(app.getHttpServer())
+            .get('/api/freelancers')
+            .query(freelancerSearch)
+            .set('Cookie', [`Authentication=${token}`])
+            .expect(200);
+
+          const freelancers = res.body;
+
+          expect(freelancers.length).toEqual(10);
+          expect(freelancers[0].username).toEqual(user.name);
+          expect(freelancers[0].position).toEqual(Position.BACK_END);
+        });
+
+        test('페이지 번호를 입력하지 않을 경우 첫 페이지를 조회', async () => {
+          const freelancerSearch: FreelancerSearch = {
+            keyword: '프론트',
+          };
+
+          const res = await request(app.getHttpServer())
+            .get('/api/freelancers')
+            .query(freelancerSearch)
+            .set('Cookie', [`Authentication=${token}`])
+            .expect(200);
+
+          const freelancers = res.body;
+
+          expect(freelancers.length).toEqual(10);
+          expect(freelancers[0].username).toEqual(user.name);
+          expect(freelancers[0].position).toEqual(Position.FRONT_END);
+        });
+
+        test('검색어와 페이지 번호에 해당하는 목록 조회', async () => {
+          const freelancerSearch: FreelancerSearch = {
+            keyword: '프론트',
+            page: 1,
+          };
+
+          const res = await request(app.getHttpServer())
+            .get('/api/freelancers')
+            .query(freelancerSearch)
+            .set('Cookie', [`Authentication=${token}`])
+            .expect(200);
+
+          const freelancers = res.body;
+
+          expect(freelancers.length).toEqual(2);
+          expect(freelancers[0].username).toEqual(user.name);
+          expect(freelancers[0].position).toEqual(Position.FRONT_END);
+        });
       });
     });
   });
